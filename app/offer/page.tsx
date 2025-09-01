b73773b42c5965494839bdf136edb2a83cce01f9
Project Admin <admin@project.local>
2025-09-01 20:21:34 +0300

    Fix arrow direction to point downward

--- app/offer/page.tsx
+++ app/offer/page.tsx
@@ -972,7 +972,7 @@ export default function OfferPage() {
                   animation: 'bounce 1s infinite'
                 }}
               >
-                <path d="M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z" />
+                <path d="M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6-6-6 1.41-1.41z" />
               </svg>
             </div>
           </div>
